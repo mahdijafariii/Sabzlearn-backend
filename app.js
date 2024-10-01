dc2e14564c10cb9b516d8c3b2882f22a3f11a2e0
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-01 18:50:30 +0330

    add default middleware

--- app.js
+++ app.js
@@ -2,4 +2,19 @@ const express = require('express')
 
 const app = express()
 
+
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const authRouter = require('./routes/auth')
+
+app.use(
+    "/courses/covers",
+    express.static(path.join(__dirname, "public", "courses", "covers"))
+);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/v1/auth", authRouter);
 module.exports = app;
---
+++ validators/register.js
@@ -0,0 +1,17 @@
+const Validator = require("fastest-validator");
+
+const v = new Validator();
+
+const schema = {
+    name: { type: "string", min: 3, max: 255 },
+    username: { type: "string", min: 3, max: 100 },
+    phone: { type: "number", max: 11 },
+    email: { type: "email", min: 10, max: 100 },
+    password: { type: "string", min: 8, max: 24 },
+    confirmPassword: { type: "equal", field: "password" },
+    $$strict: true,
+};
+
+const check = v.compile(schema);
+
+module.exports = check;
