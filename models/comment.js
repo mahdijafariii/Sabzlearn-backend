4054f2af0dd766b098f638ccacc3ec445a2f5865
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-02 21:56:27 +0330

    add comment model

---
+++ models/comment.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+    {
+        body: {
+            type: String,
+            required: true,
+        },
+        course: {
+            type: mongoose.Types.ObjectId,
+            ref: "Course",
+            required: true,
+        },
+        creator: {
+            type: mongoose.type.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        isAccept: {
+            type: Number, // 0 - 1
+            default: 0,
+        },
+        score: {
+            type: Number,
+            default: 5,
+        },
+        isAnswer: {
+            type: Number, // 0 - 1
+            required: true,
+        },
+        mainCommentID: {
+            type: mongoose.Types.ObjectId,
+            ref: "Comment",
+        },
+    },
+    { timestamps: true }
+);
+
+const model = mongoose.model("Comment", schema);
+
+module.exports = model;
