728135800c4ce5642160d71d7033fb169fb57023
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-02 22:12:16 +0330

    add uploader utils

---
+++ utils/uploader.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const multer = require("multer");
+const crypto = require("crypto");
+
+module.exports = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "..", "public", "courses", "covers"));
+    },
+    filename: (req, file, cb) => {
+        const fileName = Date.now() + String(Math.random() * 9999);
+        const ext = path.extname(file.originalname);
+        cb(null, fileName + ext);
+    },
+});
