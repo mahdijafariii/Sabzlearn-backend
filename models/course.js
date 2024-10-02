941b7933d927dcc80564ac33390b73c2eda7b5c0
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-02 22:10:27 +0330

    add course model

--- models/course.js
+++ models/course.js
@@ -1,25 +1,64 @@
-const mongoose = require('mongoose');
+const {default : mongoose} = require('mongoose');
 
 const schema = mongoose.Schema({
-        title: {
+        name: {
             type: String,
             required: true
         },
-        time: {
+        description: {
             type: String,
             required: true
         },
-        free: {
-            type: Number, // 0 -> false // 1 -> true
+        cover: {
+            type: String, // 0 -> false // 1 -> true
             required: true
         },
-        video: {
+        support: {
             type: String,
             required: true
         },
+    href: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    discount: {
+        type: Number,
+        required: true
+    },
+    categoryId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Category',
+        required :true
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref : 'User',
+        required: true
+    },
     }, {timestamps: true}
 );
 
-const model = mongoose.model('Category', schema);
+schema.virtuals('sessions',{
+    localField: '_id',
+    ref : 'Session',
+    foreignFiled : 'course'
+})
+
+schema.virtuals('comments',{
+    localField: '_id',
+    ref : 'Comment',
+    foreignFiled : 'course'
+})
+
+
+const model = mongoose.model('Course', schema);
 
 module.exports = model;
