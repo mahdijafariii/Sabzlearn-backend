0aa65748ee867004667befa1916562617d9b8fb4
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-03 17:39:52 +0330

    add answer comment router

--- routes/comment.js
+++ routes/comment.js
@@ -10,6 +10,8 @@ router.route('/add-comment').post(authMiddleware,controller.addComment);
 router.route('/:id/').delete(authMiddleware,isAdminMiddleware,controller.deleteComment);
 router.route('/:id/accept').put(authMiddleware,isAdminMiddleware,controller.acceptComment);
 router.route('/:id/reject').put(authMiddleware,isAdminMiddleware,controller.rejectComment);
+router.route('/:id/answer').post(authMiddleware,isAdminMiddleware,controller.rejectComment);
+
 
 
 module.exports = router
