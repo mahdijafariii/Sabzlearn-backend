9bccb5824eeb814e82ce8d746681f271118ca1d3
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-03 18:58:11 +0330

    add cotact model

---
+++ models/contact.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        answer: {
+            type: Number, // 0 - 1
+            required: true,
+        },
+        body: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const model = mongoose.model("Contact", schema);
+
+module.exports = model;
