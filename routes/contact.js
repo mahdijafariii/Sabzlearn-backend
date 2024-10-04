add657d38661ac0f926193eb2fd30e15b1f16778
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-04 19:03:02 +0330

    fix router

--- routes/contact.js
+++ routes/contact.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const contactsController = require("./../../controllers/v1/contact");
-const authMiddleware = require("./../../middlewares/auth");
-const isAdminMiddleware = require("./../../middlewares/isAdmin");
+const contactsController = require("./../controllers/contact");
+const authMiddleware = require("../middelware/auth");
+const isAdminMiddleware = require("../middelware/isAdmin");
 
 const router = express.Router();
 
 router.route("/").get(authMiddleware, isAdminMiddleware, contactsController.getAll)
 router.route('/').post(contactsController.create);
-router.route("/:id").delete(contactsController.remove);
+router.route("/:id").delete(authMiddleware, isAdminMiddleware,contactsController.remove);
 
-router.route("/answer").post(authMiddleware, isAdminMiddleware, contactsController.answer);
+// router.route("/answer").post(authMiddleware, isAdminMiddleware, contactsController.answer);
 
 module.exports = router;
