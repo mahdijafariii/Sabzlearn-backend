10e3b799bb5804aaa4bf4a399e19cff8dc9dd1cd
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-04 19:57:38 +0330

    add api to answer contact with nodemailer

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,6 +1,7 @@
 const contactModel = require('../models/contact')
 const {default : mongoose} = require("mongoose");
 const addCommentValidator = require("../validators/addComment");
+const nodemailer = require('nodemailer');
 
 const getAll = async (req,res)=>{
     const contacts = await contactModel.find({}).lean();
@@ -40,6 +41,38 @@ const create = async (req,res)=>{
 }
 
 const answer = async (req,res)=>{
+    const {answer , userEmail} = req.body;
+    let transporter = nodemailer.createTransport({
+        service : 'gmail',
+        auth : {
+            user : "your email",
+            pass : "your pass ! , but is not your password that you login in your email account"
+        },
+    })
+
+    const mailOptions = {
+        from : "email you want sent email with",
+        to : userEmail,
+        subject : "این ایمیل از سمت ما فرستاده شده است ",
+        text : answer
+    }
+
+    transporter.sendMail(mailOptions , async (error , info) =>{
+        if (error){
+            return res.status.json({
+                message : error
+            })
+        }
+        else {
+            const contact = await contactModel.findOneAndUpdate({email : userEmail} , {
+                answer : 1
+            })
+            return res.status.json({
+                message : 'email send successfully'
+            })
+        }
+
+    })
 
 
 }
