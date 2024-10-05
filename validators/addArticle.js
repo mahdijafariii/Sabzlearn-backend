adec8d0c5fefccd34306940af8454742ac4ab880
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-05 22:47:16 +0330

    add validator to cerate article

--- validators/addArticle.js
+++ validators/addArticle.js
@@ -4,11 +4,14 @@ const {default: mongoose} = require("mongoose");
 const v = new Validator();
 
 const schema = {
-    body: { type: "string", min: 5, max: 255 },
-    phone: { type: "string", min: 5, max: 255 },
-    email: { type: "string", min: 5, max: 255 },
-    name: { type: "string", min: 2, max: 255 },
+    title: { type: "string", min: 5, max: 255 },
+    description: { type: "string", min: 5, max: 255 },
+    body: { type: "string", min: 10, max: 255 },
+    href: { type: "string", min: 2, max: 255 },
+    categoryId: { type: "string", min: 2, max: 255 },
+    publish: { type: "number" },
 };
+
 
 const check = v.compile(schema);
 
