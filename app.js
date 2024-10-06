5fc0bb53c8d1b81da86acae6b8009dbfbef88a3a
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-06 16:47:32 +0330

    add department model

--- app.js
+++ app.js
@@ -16,14 +16,6 @@ const notificationRouter = require("./routes/notification");
 const offRouter = require("./routes/off");
 const articleRouter = require("./routes/article");
 const orderRouter = require("./routes/order");
-
-
-
-
-
-
-
-
 app.use(
     "/courses/covers",
     express.static(path.join(__dirname, "public", "courses", "covers"))
